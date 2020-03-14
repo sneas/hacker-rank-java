006aeb986286248dbf059a8fa132c6855be079af
Dima Snisarenko <snisarenkodima@gmail.com>
2020-03-14 10:30:04 +0100

    feat: add test structure

--- src/io/github/sneas/Main.java
+++ src/io/github/sneas/Main.java
@@ -1,8 +1,22 @@
 package io.github.sneas;
 
-public class Main {
+class Node {
+    public Node(int val, int ht) {
+        this.val = val;
+        this.ht = ht;
+    }
 
+    public Node left;
+    public Node right;
+    public int val;
+    public int ht;
+}
+
+public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        Node tree = new Node(3, 2);
+        tree.left = new Node(2, 0);
+        tree.right = new Node(4, 1);
+        tree.right.right = new Node(5, 0);
     }
 }
