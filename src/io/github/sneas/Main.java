a0a58b4f2adc192a18211d990c6f97ee12ca6063
Dima Snisarenko <snisarenkodima@gmail.com>
2020-03-15 07:49:56 +0100

    chore: change package name to io.github.sneas

--- src/io/github/sneas/Main.java
+++ src/io/github/sneas/Main.java
@@ -3,6 +3,6 @@ package io.github.sneas;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.print("It's working");
     }
 }
