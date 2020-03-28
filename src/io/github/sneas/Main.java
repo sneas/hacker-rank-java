44a4d3a8a4afaa070751d9deae5b264e3968a0a8
Dima Snisarenko <snisarenkodima@gmail.com>
2020-03-28 09:09:11 +0100

    Add largest rectangle

--- src/io/github/sneas/Main.java
+++ src/io/github/sneas/Main.java
@@ -1,8 +1,53 @@
 package io.github.sneas;
 
+import java.util.*;
+
 public class Main {
 
+    static class Sector {
+        public final int position;
+        public final int height;
+
+        Sector(int position, int height) {
+            this.position = position;
+            this.height = height;
+        }
+    }
+
+    static long largestRectangle(int[] h) {
+        List<Sector> stack = new Stack<Sector>();
+
+        int max = 0;
+
+        for (int i = 0; i < h.length; i++) {
+            int value = h[i];
+            int position = i;
+            Sector last = stack.isEmpty() ? null : stack.get(stack.size() - 1);
+            while (last != null && last.height > value) {
+                int square = last.height * (i - last.position);
+                if (square > max) {
+                    max = square;
+                }
+                position = last.position;
+                stack.remove(stack.size() - 1);
+                last = stack.isEmpty() ? null : stack.get(stack.size() - 1);
+            }
+            stack.add(new Sector(position, value));
+        }
+
+        while(!stack.isEmpty()) {
+            Sector last = stack.remove(stack.size() - 1);
+            int square = last.height * (h.length - last.position);
+            if (square > max) {
+                max = square;
+            }
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.print("It's working");
+        long result  = largestRectangle(new int[]{8979, 4570, 6436, 5083, 7780, 3269, 5400, 7579, 2324, 2116});
+        System.out.println(result);
     }
 }
