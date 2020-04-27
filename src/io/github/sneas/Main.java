f98449d006e72086f0bd96c5b9a600929578faaa
Dima Snisarenko <snisarenkodima@gmail.com>
2020-04-27 10:48:06 +0200

    Add forming-a-magic-square

--- src/io/github/sneas/Main.java
+++ src/io/github/sneas/Main.java
@@ -3,54 +3,88 @@ package io.github.sneas;
 import java.util.*;
 
 public class Main {
+    static void generateSquare(int[][] s, LinkedList<Integer> results, LinkedList<Integer> availableItems, int index, int[] combination) {
+        if (index == 9) {
+            int magicSum = 0;
+            int steps = 0;
+            int diagonal1 = 0;
+            for (int i = 0; i < 3; i++) {
+                int horizontalSum = 0;
+                int verticalSum = 0;
+                for (int j = 0; j < 3; j++) {
+                    int magicIJ = combination[i * 3 + j];
+                    steps += Math.abs(s[i][j] - magicIJ);
+                    horizontalSum += magicIJ;
+                    verticalSum += combination[j * 3 + i];
+                    if (i == j) {
+                        diagonal1 += combination[i * 3 + j];
+                    }
+                }
+                if (magicSum == 0) {
+                    magicSum = horizontalSum;
+                }
 
-    static long solve(int[] arr) {
-        Stack<Integer> stack = new Stack<Integer>();
-        long[] left = new long[arr.length];
+                if (horizontalSum != magicSum) {
+                    return;
+                }
 
-        for (int i = 0; i < arr.length; i++) {
-            long ai = arr[i];
-
-            while(!stack.isEmpty() && arr[stack.peek()] <= ai) {
-                stack.pop();
+                if (verticalSum != magicSum) {
+                    return;
+                }
             }
 
-            long li = stack.isEmpty() ? 0 : stack.peek() + 1;
-            left[i] = li;
+            if (diagonal1 != magicSum) {
+                return;
+            }
 
-            stack.push(i);
+            int diagonal2 = 0;
+
+            for (int i = 0; i < 3; i++) {
+                diagonal2 += combination[i * 3 + (2 - i)];
+            }
+
+            if (diagonal2 != magicSum) {
+                return;
+            }
+
+            results.add(steps);
+
+            return;
         }
 
-        stack.clear();
+        for (Integer item: availableItems) {
+            LinkedList<Integer> newAvailableItems = (LinkedList)availableItems.clone();
+            newAvailableItems.remove(item);
+            combination[index] = item;
+            generateSquare(s, results, newAvailableItems, index + 1, combination);
+        }
+    }
 
-        long max = 0;
+    static int formingMagicSquare(int[][] s) {
+        LinkedList<Integer> availableItems = new LinkedList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        int index = 0;
+        int[] combination = new int[9];
+        LinkedList<Integer> results = new LinkedList<Integer>();
 
-        for (int i = arr.length - 1; i >= 0; i--) {
-            int ai = arr[i];
+        generateSquare(s, results, availableItems, index, combination);
 
-            while(!stack.isEmpty() && arr[stack.peek()] <= ai) {
-                stack.pop();
+        int min = 10000000;
+
+        for (int steps: results) {
+            if (steps < min) {
+                min = steps;
             }
-
-            long ri = stack.isEmpty() ? 0 : stack.peek() + 1;
-
-            long indexProduct = ri * left[i];
-
-            if (indexProduct > max) {
-                max = indexProduct;
-            }
-
-            stack.push(i);
         }
 
-        return max;
+        return min;
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[100000];
-        Arrays.fill(arr, 1000000000);
-        arr[50000] = 1000;
-        long result  = solve(arr);
+        int result  = formingMagicSquare(new int[][]{
+                {4, 8, 2},
+                {4, 5, 7},
+                {6, 1, 6}
+        });
         System.out.println(result);
     }
 }
