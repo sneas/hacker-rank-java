78fef668a90cd689a585ed9649d83b752c181d23
Dima Snisarenko <snisarenkodima@gmail.com>
2020-05-17 10:57:47 +0200

    Add hourglass solution

--- src/io/github/sneas/Main.java
+++ src/io/github/sneas/Main.java
@@ -3,31 +3,44 @@ package io.github.sneas;
 import java.util.*;
 
 public class Main {
-    static int minimumSwaps(int[] arr) {
-        int result = 0;
-        Hashtable<Integer, Integer> h = new Hashtable<>();
+    static int hourglassSum(int[][] arr) {
+        int max = -1000000;
+        for (int i = 1; i<= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (i == 4 && j == 3) {
+                    int k = 5;
+                }
+                int sum = arr[i - 1][j - 1]+ arr[i-1][j] + arr[i-1][j+1]
+                        + arr[i][j]
+                        + arr[i+1][j - 1] + arr[i + 1][j] + arr[i+1][j+1];
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == i+1) {
-                continue;
+                if (sum > max) {
+                    max = sum;
+                }
             }
-
-            while (h.containsKey(arr[i]) && arr[i] != i+1) {
-                result++;
-                int ai = arr[i];
-                arr[i] = h.get(ai);
-                h.remove(ai);
-                h.put(i+1, arr[i]);
-            }
-
-            h.put(i+1, arr[i]);
         }
-        return result;
+
+        return max;
     }
 
     public static void main(String[] args) {
-        int result  = minimumSwaps(new int[]{2, 3, 4, 1, 5});
-//        int result  = minimumSwaps(new int[]{1, 3, 5, 2, 4, 6, 7});
+//        int result  = hourglassSum(new int[][]{
+//            {1, 1, 1, 0, 0, 0,},
+//            {0, 1, 0, 0, 0, 0,},
+//            {1, 1, 1, 0, 0, 0,},
+//            {0, 0, 2, 4, 4, 0,},
+//            {0, 0, 0, 2, 0, 0,},
+//            {0, 0, 1, 2, 4, 0,},
+//        });
+        
+        int result  = hourglassSum(new int[][]{
+            {-1, -1, 0, -9, -2, -2,},
+            {-2, -1, -6, -8, -2, -5,},
+            {-1, -1, -1, -2, -3, -4,},
+            {-1, -9, -2, -4, -4, -5,},
+            {-7, -3, -3, -2, -9, -9,},
+            {-1, -3, -1, -2, -4, -5,},
+        });
         System.out.println(result);
     }
 }
