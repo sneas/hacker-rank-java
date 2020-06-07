a9100d6e15b2a244337c163b5cdf04b572763a08
Dima Snisarenko <snisarenkodima@gmail.com>
2020-06-07 09:10:25 +0200

    add count triplets solution

--- src/io/github/sneas/Main.java
+++ src/io/github/sneas/Main.java
@@ -1,33 +1,65 @@
 package io.github.sneas;
 
+import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
 
 public class Main {
-    // Complete the twoStrings function below.
-    static String twoStrings(String s1, String s2) {
-        String l = s1.length() > s2.length() ? s1 : s2;
-        String s = s1.length() <= s2.length() ? s1 : s2;
-        Hashtable<Character, Boolean> h = new Hashtable<>();
+    static long countTriplets(List<Long> arr, long r) {
+        long result = 0;
 
-        for(Character c: l.toCharArray()) {
-            h.put(c, true);
-        }
+        Hashtable<Long, Long> pairs = new Hashtable<>();
+        Hashtable<Long, Long> amount = new Hashtable<>();
 
-        for (Character c: s.toCharArray()) {
-            if (h.getOrDefault(c, false)) {
-                return "YES";
+        for (long number: arr) {
+            result += pairs.getOrDefault(number / r, 0L);
+
+            if (pairs.containsKey(number)) {
+                pairs.put(number, pairs.get(number)
+                        + amount.getOrDefault(number / r, 0L));
             }
+
+            pairs.put(number * r, pairs.getOrDefault(number * r, 0L));
+
+            amount.put(number, amount.getOrDefault(number, 0L) + 1);
         }
 
-        return "NO";
+        return result;
     }
 
-    public static void main(String[] args) {
-        String result = twoStrings(
-                "hello",
-                "world"
-        );
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("/Users/dima/Documents/hacker-rank-java/t2.txt"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        System.out.println(result);
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
+
+//    public static void main(String[] args) {
+//        long[] arr = {1, 3, 9, 9, 27, 81};
+////        long[] arr = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,};
+//
+//        List<Long> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+//
+//        long result = countTriplets(list, 3);
+//
+//        System.out.println(result);
+//    }
 }
