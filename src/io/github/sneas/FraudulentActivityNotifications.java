7ea8c2d97fb9e2ee1e6b2e1bb0ef75754ee5281a
Dima Snisarenko <snisarenkodima@gmail.com>
2020-06-21 14:05:57 +0200

    solve fraudlent-activity-notifications

--- src/io/github/sneas/FraudulentActivityNotifications.java
+++ src/io/github/sneas/FraudulentActivityNotifications.java
@@ -1,17 +1,17 @@
 package io.github.sneas;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Queue;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class FraudulentActivityNotifications {
     static int activityNotifications(int[] expenditure, int d) {
         int result = 0;
 
-        int[] counts = new int[200];
-        int[] indexes = new int[200];
-        int[] sorted = new int[d];
+        int[] freqs = new int[200];
         int doubleMedian = 0;
         Queue<Integer> q = new ArrayDeque<Integer>();
         int last = 0;
@@ -23,43 +23,91 @@ public class FraudulentActivityNotifications {
 
             if (q.size() == d) {
                 last = q.remove();
-                counts[last]--;
+                freqs[last]--;
             }
 
             q.add(expense);
-            counts[expense]++;
+            freqs[expense]++;
 
             if (q.size() < d) {
                 continue;
             }
 
-            int total = 0;
-            for (int i = 0; i < counts.length; i++) {
-                total += counts[i];
-                indexes[i] = total;
-            }
-
-            ArrayList<Integer> list = new ArrayList<>(q);
-            Hashtable<Integer, Integer> h = new Hashtable<>();
-
-            for (int i: list) {
-                sorted[h.getOrDefault(i, indexes[i] - 1)] = i;
-                h.put(i, h.getOrDefault(i, indexes[i] - 1) - 1);
-            }
-
-            if (d % 2 == 0) {
-                doubleMedian = sorted[d / 2] + sorted[d / 2  + 1];
-            } else {
-                doubleMedian = sorted[d / 2] * 2;
-            }
+            doubleMedian = getDoubleMedian(freqs, d);
         }
 
         return result;
     }
 
-    public static void main(String[] args) {
-//        int result = activityNotifications(new int[]{1, 2, 3, 4, 4}, 4);
-        int result = activityNotifications(new int[]{2, 3, 4, 2, 3, 6, 8, 4, 5}, 5);
-        System.out.println(result);
+    public static int getDoubleMedian(int[] freqs, int d) {
+        int total = 0;
+        int doubleMedian = 0;
+
+        if (d % 2 == 1) {
+            for (int exp = 0; exp < freqs.length; exp++) {
+                total += freqs[exp];
+                if (total > d / 2) {
+                    return exp * 2;
+                }
+            }
+        }
+
+        int first = -1;
+        int second = -1;
+
+        for (int exp = 0; exp < freqs.length; exp++) {
+            total += freqs[exp];
+
+            if(total == d/2)
+            {
+                first = exp;
+            } else if(total > d/2) {
+                if(first < 0 ) {
+                    first = exp;
+                }
+                second = exp;
+
+                return first + second;
+            }
+        }
+
+        return doubleMedian;
     }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(new File("input/fraudlent-activity-notifications/1.txt"), StandardCharsets.UTF_8.name());
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] expenditure = new int[n];
+
+        String[] expenditureItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int expenditureItem = Integer.parseInt(expenditureItems[i]);
+            expenditure[i] = expenditureItem;
+        }
+
+        int result = activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+//    public static void main(String[] args) {
+////        int result = activityNotifications(new int[]{1, 2, 3, 4, 4}, 4);
+//        int result = activityNotifications(new int[]{2, 3, 4, 2, 3, 6, 8, 4, 5}, 5);
+//        System.out.println(result);
+//    }
 }
